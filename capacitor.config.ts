5a9cd96de82957c0ebbadf86f04063cb6bd591f1
pollrecords <pauluvencee@gmail.com>
2026-03-06 08:24:35 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'App diabetes',
+  webDir: 'www'
+};
+
+export default config;
