b41080319bf253cb83d79787c75f68f055ab86a1
Xanderl-xan <orlando.alvarado8771@alumnos.udg.mx>
2026-03-12 21:30:08 -0600

    Nueva pagina/semaforo

---
+++ src/app/paginas/semaforo/semaforo.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-semaforo',
+  templateUrl: './semaforo.page.html',
+  styleUrls: ['./semaforo.page.scss'],
+  standalone: false,
+})
+export class SemaforoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
