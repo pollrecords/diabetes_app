185e76aa2a4d61cb1636eaf0bfc813f4141a1809
Xanderl-xan <orlando.alvarado8771@alumnos.udg.mx>
2026-03-20 07:31:59 -0600

    Cree la paguina info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'semaforo',
     loadChildren: () => import('./paginas/semaforo/semaforo.module').then( m => m.SemaforoPageModule)
   },
+  {
+    path: 'info',
+    loadChildren: () => import('./paginas/info/info.module').then( m => m.InfoPageModule)
+  },
 
 ];
 
